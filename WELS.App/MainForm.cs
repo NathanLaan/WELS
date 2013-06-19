9c504f6904cdfe160c214d27069bc2e4d3eaeca8
nathan laan <nathanlaan@gmail.com>
2013-06-18 20:56:34 -0600

    Working on UI. Build broken.

--- WELS.App/MainForm.cs
+++ WELS.App/MainForm.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+
+using System.Threading;
+using System.Diagnostics;
 
 namespace WELS.App
 {
@@ -20,5 +19,61 @@ namespace WELS.App
         {
             this.lsvSearchList.Items.Add(new ListViewItem(Guid.NewGuid().ToString()));
         }
+
+        #region Threading
+
+        private Thread eventLogThread;
+        private bool threadRunning = false;
+
+
+        private class ThreadSafeInt
+        {
+            public Control Control { get; set; }
+            public int Count { get; set; }
+            private delegate void InternalDelegate();
+            public ThreadSafeInt(Control control)
+            {
+                this.Control = control;
+            }
+            public void Increment()
+            {
+                if (this.Control.InvokeRequired)
+                {
+                    this.Control.Invoke(new InternalDelegate(Increment), new object[] { });
+                }
+                else
+                {
+                    this.Count++;
+                }
+            }
+        }
+
+        #endregion
+
+
+
+        private void ProcessEventLog()
+        {
+            try
+            {
+                var log = new EventLog(this.txtEventLogName.Text, this.txtServerName.Text);
+                int count = 0;
+                foreach (EventLogEntry entry in log.Entries)
+                {
+                    if (!threadRunning)
+                    {
+                        break;
+                    }
+                    UpdateProgress(log.Entries.Count, count++);
+                    
+                }
+            }
+            catch (Exception e)
+            {
+                this.AppendText(e.ToString());
+            }
+        }
+
+
     }
 }
