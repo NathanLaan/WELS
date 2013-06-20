aa313386bd3901fbaedeac8cc565265e0f737637
nathanlaan@gmail.com <nathanlaan@gmail.com>
2013-06-19 23:02:53 -0600

    Working on search classes.

---
+++ WELS.App/SearchResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WELS.App
+{
+    class SearchResult
+    {
+
+        public DateTime Timestamp { get; set; }
+
+        public string Message { get; set; }
+
+        public string ReplacementString { get; set; }
+
+    }
+}
